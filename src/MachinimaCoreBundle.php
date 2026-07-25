e91318858b6712e872e97e46b10035ac10eecfc3
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-07-25 19:30:19 +0300

    feat: Use AutoServicesTrait to load bundle services

--- src/MachinimaCoreBundle.php
+++ src/MachinimaCoreBundle.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Morfeditorial\MachinimaCoreBundle;
 
+use Morfeditorial\MachinimaCoreBundle\Bundle\AutoServicesTrait;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class MachinimaCoreBundle extends AbstractBundle {}
+class MachinimaCoreBundle extends AbstractBundle
+{
+    use AutoServicesTrait;
+}
